9ae32f70987caa0185c6eb8762820e880e1f5602
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-05-03 14:11:15 +0200

    feat: new utility, small changes to command executor

---
+++ src/utils/Errors.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    premiumRequired(message) {
+        return message.channel.send(
+            new MessageEmbed()
+                .setTitle('Вы наткнулись на бонусную возможность!')
+                .setDescription('К сожалению, данная возможность доступна только за поддержку нашего проекта. Посетите [данную страницу](https://flamebot.ru/donate) для дополнительной информации.\n\nЕсли вы уже поддержали нас, то воспользуйтесь командой `bonus` для активации бонусных-возможностей на данном сервере.')
+                .setColor('ffa550')
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .setTimestamp()
+        );
+    }
+}
