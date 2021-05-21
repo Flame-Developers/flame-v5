f96355d5af96ff1636e06acc1e2e3915351d0d0b
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-05-21 19:03:51 +0200

    feat(bot): updates and improvements to getHelp function

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -1,32 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-  getHelp(message, command) {
-    const cmd = message.client.commands.get(command);
-    const categories = {
-      general: 'Основное',
-      moderation: 'Модерация',
-      music: 'Музыка',
-      economy: 'Экономика',
-      settings: 'Настройки',
-      leveling: 'Уровни',
-    };
-
-    if (!cmd || cmd.name === 'eval') return null;
-
-    return message.channel.send(
-      new MessageEmbed()
-        .setTitle('Информация о команде')
-        .setThumbnail(message.client.user.avatarURL())
-        .setColor('ffa500')
-        .setFooter(message.guild.name, message.guild.iconURL())
-        .setTimestamp()
-        .addField('📜 Описание', cmd.description)
-        .addField('📁 Категория', categories[cmd.category], true)
-        .addField('📎 Псевдонимы', cmd.aliases.length ? cmd.aliases.map((a) => `\`${a}\``) : 'Отсутствуют', true)
-        .setDescription(`\`\`\`${cmd.usage}\`\`\``),
-    );
-  },
   formatNumber(number) {
     return new Intl.NumberFormat('en-US').format(number);
   },
@@ -36,6 +10,9 @@ module.exports = {
     // eslint-disable-next-line no-param-reassign
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  percentage(percent, whole) {
+    return parseInt((percent * whole) / 100.0);
   },
   msToTime(duration) {
     let seconds = parseInt((duration / 1000) % 60);
@@ -52,5 +29,22 @@ module.exports = {
     if (isMs) n = ~~(n / 1000);
     // eslint-disable-next-line
     return `${n} ${text[ n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2 ]}`;
+  },
+  getHelp(message, cmd) {
+    const command = message.client.commands.get(cmd);
+    if (!command || !command?.category) return null;
+
+    return message.channel.send(
+      new MessageEmbed()
+        .setTitle('Информация о команде')
+        .setThumbnail(message.client.user.avatarURL())
+        .setColor('ffa500')
+        .setFooter(message.guild.name, message.guild.iconURL())
+        .setTimestamp()
+        .addField('📜 Описание', command.description)
+        .addField('⏱️ Задержка', module.exports.locale(command.cooldown, ['секунда', 'секунды', 'секунд']), true)
+        .addField('📎 Псевдонимы', command.aliases.length ? command.aliases.map((a) => `\`${a}\``) : 'Отсутствуют', true)
+        .setDescription(`\`\`\`${command.usage}\`\`\``),
+    );
   },
 };
