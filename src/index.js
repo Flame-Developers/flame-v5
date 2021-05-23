fd7428baeb73fcb3460b96f2a80439e8fd127157
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-05-23 22:32:24 +0200

    chore: remove useless comments

--- src/index.js
+++ src/index.js
@@ -17,14 +17,6 @@ const client = new FlameClient({
   disableMentions: 'all',
 });
 
-/* Discord.js v13 or higher:
-const client = new FlameClient({
-  intents: Intents.ALL,
-  allowedMentions: {
-    parse: [],
-  },
-}); */
-
 // eslint-disable-next-line no-console,no-underscore-dangle
 client._launch().catch(console.error);
 client.on('shardDisconnect', (data, id) => // eslint-disable-next-line
