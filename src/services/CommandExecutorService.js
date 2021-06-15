e92e3a1ed3fa836f2a7fb3a0b83130bef1ae8048
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-06-15 16:56:35 +0200

    chore(CommandExecutorService): add some checks & update error handling

--- src/services/CommandExecutorService.js
+++ src/services/CommandExecutorService.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
 
-const { Collection } = require('discord.js');
+const { MessageEmbed, Collection } = require('discord.js');
 const { permissions } = require('../utils/Constants');
 const { premiumRequired } = require('../utils/Errors');
 
 const cooldown = new Collection();
+const executions = new Map();
 
 class CommandsExecutorService {
   constructor(message, client) {
@@ -28,26 +29,40 @@ class CommandsExecutorService {
     const command = await this.findCommand(cmd);
 
     if (cooldown.has(this.message.author.id) && cooldown.get(this.message.author.id) === command?.name) return this.message.react('⏱️').catch();
+    if (executions.has(this.message.author.id)) return this.message.react('<a:processing:853669211028324402>').catch();
+
     if (command) {
+      executions.set(this.message.author.id, true);
       if (data.disabledCommands?.includes(command.name)) {
         return data?.settings?.answerOnDisabledCommands
-          ? this.message.reply('Данная команда была отключена администратором на данном сервере :no_entry:')
+          ? this.message.fail('Данная команда была отключена администратором на данном сервере.')
           : null;
       }
-      if (!this.message.guild.me.permissionsIn(this.message.channel).has('EMBED_LINKS')) return this.message.reply(`Упс, кажется, что у меня нет прав на встраивание ссылок в данном канале. Выдайте мне пожалуйста данную возможность, иначе я не смогу корректно работать и выполнять команды :no_entry:`);
+      if (!this.message.guild.me.permissionsIn(this.message.channel).has('EMBED_LINKS')) return this.message.fail(`Упс, кажется, что у меня нет прав на встраивание ссылок в данном канале. Выдайте мне пожалуйста данную возможность, иначе я не смогу корректно работать и выполнять команды.`);
       if (command.premium && !await this.message.guild.hasPremium()) return premiumRequired(this.message);
 
       if (command.clientPermissions.length > 0 && command.clientPermissions.some((permission) => !this.message.guild.me.permissions.has(permission))) return this.message.fail(`У меня недостаточно прав для выполнения данного действия. Необходимые права: ${command.clientPermissions.map((r) => `\`${permissions[r]}\``).join(', ')}.`);
       if ((command.userPermissions.length > 0 && command.userPermissions.some((permission) => !this.message.member.permissions.has(permission))) && !this.message.member.roles.cache.has(data?.moderator)) return this.message.fail(`У вас недостаточно прав для выполнения данного действия. Необходимые права: ${command.userPermissions.map((r) => `\`${permissions[r]}\``).join(', ')}.`);
 
       try {
-        command.run(this.message, args);
+        await command.run(this.message, args);
         if (data?.settings?.clearCommandCalls) await this.message.delete().catch();
       } catch (error) {
-        this.client.emit('commandError', error, this.message);
+        this.message.reply(
+          new MessageEmbed()
+            .setTitle('Упс, что-то пошло не так…')
+            .setDescription(
+              'При выполнении данной команды возникла ошибка. Попробуйте пожалуйста позже, или обратитесь на сервер поддержки.',
+            )
+            .setColor('#ff3333')
+            .setFooter(this.message.guild.name, this.message.guild.iconURL())
+            .setTimestamp(),
+        )
+        console.error(error);
       }
+      executions.delete(this.message.author.id);
 
-      if (!await this.message.guild.hasPremium()) {
+      if (!await this.message.guild.hasPremium() || command.name === 'reset-economy') {
         cooldown.set(this.message.author.id, command.name);
         setTimeout(() => cooldown.delete(this.message.author.id), command.cooldown * 1000);
       }
