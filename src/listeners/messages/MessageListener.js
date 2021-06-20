2a896e2eabb89e178dcdd3a443a45dd96b605b7d
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-06-20 10:58:17 +0200

    feat(bot): answer on mentions with providing documentation link

--- src/listeners/messages/MessageListener.js
+++ src/listeners/messages/MessageListener.js
@@ -16,6 +16,9 @@ class MessageListener extends FlameListener {
     if (await InviteDetectorService.hasInvites(message)) {
       await new AntiInviteService(message.client, message).applyActions();
     }
+    if ([`<@!${client.user.id}>`, `<@${client.user.id}>`].some((mention) => message.content.startsWith(mention))) {
+      message.reply(':wave: Привет! Для того, чтобы посмотреть полный список команд, воспользуйтесь командой `help`.\nНе забудьте также посетить документацию: https://docs.flamebot.ru.');
+    }
     const user = await client.database.collection('guildusers').findOne({ guildID: message.guild.id, userID: message.author.id });
     if (!user && !message.author.bot) {
       // eslint-disable-next-line consistent-return
