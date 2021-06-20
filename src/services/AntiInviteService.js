c78f45cffe9c42a67347e450302a70932f33b249
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-06-20 11:39:10 +0200

    fix(AntiInviteService): avoid getting errors if something is undefined

--- src/services/AntiInviteService.js
+++ src/services/AntiInviteService.js
@@ -9,11 +9,11 @@ class AntiInviteService {
 
   async applyActions() {
     const data = await this.client.database.collection('guilds').findOne({ guildID: this.message?.guild.id });
-    if (data?.antiInvite.enabled && this.message.guild.me.permissions.has('ADMINISTRATOR')) {
+    if (data.antiInvite?.enabled && this.message.guild.me.permissions.has('ADMINISTRATOR')) {
       if (this.message.member.permissions.has('ADMINISTRATOR')) return false;
 
       this.message.delete().catch(() => {});
-      if (data?.antiInvite.message) this.message.channel.send(
+      if (data.antiInvite?.message) this.message.channel.send(
         StringParserUtil.parse(data.antiInvite.message, {
           'user': this.message.author,
           'user.tag': this.message.author.tag,
