b77e9c5384a9a1e13d3ada3202bfcf3e75b54091
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-06-25 20:51:02 +0200

    chore(GuildCreateListener): update welcome message

--- src/listeners/guilds/GuildCreateListener.js
+++ src/listeners/guilds/GuildCreateListener.js
@@ -10,8 +10,8 @@ class GuildCreateListener extends FlameListener {
     const data = await client.database.collection('guilds').findOne({ guildID: guild.id });
 
     if (!data) {
-      guild?.channels.cache.filter((c) => guild.me.permissionsIn(c).has('SEND_MESSAGES') && c.type == 'text').first().send(
-        `Привет, спасибо что добавили меня на **${guild.name}**! :wave:\nДля того чтобы начать работать со мной, загляните в документацию, там вы найдете много чего полезного: <https://docs.flamebot.ru>\nЕсли вы хотите узнать о боте и его функционале, обязательно посетите наш сайт: https://flamebot.ru\n\nЕсли у вас все еще остались вопросы, либо возникли трудности, обязательно обращайтесь на сервер поддержки. Мы постараемся помочь вам в любую минуту :heart:\nhttps://discord.gg/7FUJPRCsw8`,
+      guild?.channels.cache.filter((c) => guild.me.permissionsIn(c).has('SEND_MESSAGES') && c.type === 'text').first().send(
+        `Привет, спасибо что добавили меня на **${guild.name}**! :wave:\nДля того чтобы начать работать со мной, загляните в документацию, там вы найдете много чего полезного: <https://docs.flamebot.ru>\nУзнать о боте и его функционале вам позволит наш сайт: https://flamebot.ru\n\nЕсли у вас все еще остались вопросы, либо возникли трудности, обязательно обращайтесь на сервер поддержки. Мы постараемся помочь вам в любую минуту :heart:\nВы можете также поддержать проект материально, и открыть для себя бонусные возможности. Для этого, перейдите на страницу <https://flamebot.ru/donate>.\n\nhttps://discord.gg/7FUJPRCsw8`,
       );
       return client.database.collection('guilds').updateOne({ guildID: guild.id }, { $set: GuildSchema }, { upsert: true });
     }
