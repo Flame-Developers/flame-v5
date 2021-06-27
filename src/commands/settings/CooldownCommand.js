a92c1b741884b5700681b4375d91349131838c4f
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-06-27 22:13:24 +0200

    chore(CooldownCommand): update limits

--- src/commands/settings/CooldownCommand.js
+++ src/commands/settings/CooldownCommand.js
@@ -35,7 +35,7 @@ class CooldownCommand extends FlameCommand {
         // eslint-disable-next-line
         const time = ms(args[2]);
         if (!time) return getHelp(message, this.name);
-        if (time < 300000 || time > 7 * 24 * 60 * 60 * 1000) return message.fail('Время задержки должно быть между **5-ти минут** и **одной недели**.');
+        if (time < 300000 || time > 1000 * 60 * 60 * 24) return message.fail('Время задержки должно быть между **5-ти минут** и **одними сутками**.');
 
         message.client.database.collection('guilds').updateOne({ guildID: message.guild.id }, {
           $set: {
