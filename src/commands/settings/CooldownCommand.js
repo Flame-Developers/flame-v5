e6ef32435d90d0c1c58ee4409377bac088b420a6
Z10kk <87130324+Z10kk@users.noreply.github.com>
2021-07-08 15:23:08 +0300

    Update CooldownCommand.js
    Grammatical correction.

--- src/commands/settings/CooldownCommand.js
+++ src/commands/settings/CooldownCommand.js
@@ -30,12 +30,12 @@ class CooldownCommand extends FlameCommand {
       case 'set':
         // eslint-disable-next-line
         const command = args[1];
-        if (!data.cooldown[command]) return message.fail('Указанной вами команды не существует либо вы не имеете прав на её редактирование.');
+        if (!data.cooldown[command]) return message.fail('Указанной вами команды не существует либо у вас недостаточно прав на её редактирование.');
 
         // eslint-disable-next-line
         const time = ms(args[2]);
         if (!time) return getHelp(message, this.name);
-        if (time < 300000 || time > 1000 * 60 * 60 * 24) return message.fail('Время задержки должно быть между **5-ти минут** и **одними сутками**.');
+        if (time < 300000 || time > 1000 * 60 * 60 * 24) return message.fail('Время задержки должно быть между **5-тью минутами** и **одними сутками**.');
 
         message.client.database.collection('guilds').updateOne({ guildID: message.guild.id }, {
           $set: {
