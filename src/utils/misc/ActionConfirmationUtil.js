ab5dd4fa50d65b80d6308efd6e66f779bc88158c
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-07-09 21:27:21 +0200

    feat(ActionConfirmationUtil): use ephemeral messages for interactions responses

--- src/utils/misc/ActionConfirmationUtil.js
+++ src/utils/misc/ActionConfirmationUtil.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const { MessageEmbed } = require('discord.js');
+const InteractionResponse = require('../interactions/InteractionResponse');
 
 class ActionConfirmationUtil {
   constructor(client, user) {
@@ -38,7 +39,10 @@ class ActionConfirmationUtil {
     });
     return await new Promise((resolve) => {
       this._client.cache.buttons.set(this.message.id, (res) => {
-        if (res.member.user.id === this.user?.id && res.data.custom_id === 'yes') {
+        if (res.member.user.id !== this.user?.id)
+          return new InteractionResponse(this._client).send(res, 'Взаимодействовать с кнопками может только тот, кто вызвал команду.', { flags: 64 });
+
+        if (res.data.custom_id === 'yes') {
           resolve(true);
         }
         this._client.api.channels(channel.id).messages(this.message.id).delete();
