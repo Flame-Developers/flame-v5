e9c7fd38007bdf73aafb2130ae29e991fd9bb7b1
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-07-10 10:44:10 +0200

    fix(VoiceStateUpdateListener): stop using deleted FlameGuild#hasPremium() method

--- src/listeners/VoiceStateUpdateListener.js
+++ src/listeners/VoiceStateUpdateListener.js
@@ -6,7 +6,7 @@ class VoiceStateUpdateListener extends FlameListener {
   }
 
   async run(client, oldState, newState) {
-    if (await oldState.guild.hasPremium()) return;
+    if (await oldState.guild.cache?.premium) return;
 
     const dispatcher = client.queue.get(newState.guild.id);
     if (dispatcher) {
