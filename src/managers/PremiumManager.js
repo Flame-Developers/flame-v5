71cefa77dbdf28d3305977878c141998c900983a
TheFerryn <volodymyr.germany.2020@gmail.com>
2021-07-20 19:39:39 +0200

    fix(PremiumManager): update guild cache by canceling premium status

--- src/managers/PremiumManager.js
+++ src/managers/PremiumManager.js
@@ -3,16 +3,19 @@ const BaseManager = require('../structures/BaseManager');
 class PremiumManager extends BaseManager {
   constructor(client) {
     super('subscriptions', client);
+    this.client = client;
   }
 
   async cancelPremiumStatus(data) {
     if (!data || !await this.find(data)) return null;
+
     for (const id of data?.premiumGuilds) {
       this.client.database.collection('guilds').updateOne({ guildID: id }, {
         $set: {
           premium: false,
         },
       });
+      this.client.guilds.cache.get(id).cache?.set('premium', false);
     }
     return this.delete(data);
   }
